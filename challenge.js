3237fcf6bc413090a5c3e5b84b8bfab6dc4a5dda
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-05-27 18:40:42 +0300

    Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+// values to three variables
+let x = 10;
+let y = 30;
+let z = 20;
+
+// largest number using if statements
+
+if (x >= y && x >= z) {
+  console.log("The largest number out of the three numbers is " + x);
+} else if (y >= x && y >= z) {
+  console.log("The largest number out of the three numbers is " + y);
+} else {
+  console.log("The largest number out of the three numbers is " + z);
+}
